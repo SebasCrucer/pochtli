f5762108bcf99ad5c5ef5cd7f77fbada741ec999
Sebastian <diego.cruz.cervantes@gmail.com>
2024-04-20 03:01:17 -0600

    Update CSS styles and components

--- front_end/src/components/PageContainer.tsx
+++ front_end/src/components/PageContainer.tsx
@@ -2,7 +2,7 @@ import { useNavigate, useNavigation } from 'react-router-dom';
 import './PageContainer.css'
 
 export const PageContainer = ({ from, title, children, actions }: {
-    from: string,
+    from?: string,
     title: string,
     children: React.ReactNode,
     actions?: React.ReactNode
@@ -13,14 +13,17 @@ export const PageContainer = ({ from, title, children, actions }: {
     return (
         <section className="PageContainer">
             <div>
-                <button
-                    className='PageContainer-Options-back'
-                    onClick={(e) => { e.preventDefault(); navigate(from) }}
-                    disabled={(state === 'submitting')}
-                >
-                    <box-icon name='chevron-left' />
-                    Volver
-                </button>
+                {
+                    from &&
+                    <button
+                        className='PageContainer-Options-back'
+                        onClick={(e) => { e.preventDefault(); navigate(from) }}
+                        disabled={(state === 'submitting')}
+                    >
+                        <box-icon name='chevron-left' />
+                        Volver
+                    </button>
+                }
             </div>
             <div className='PageContainerCont'>
                 <header className="PageContainerHeader">

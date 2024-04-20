dfbe764c1a2a7c48f4d08a9c9eb2c4858eb1a0b1
ZiftK <alvaro.cab.lag@gmail.com>
2024-04-20 02:18:47 -0600

    Add: general query code

--- api/query.py
+++ api/query.py
@@ -1,17 +1,47 @@
 import pandas as pd
+import re
 import sys
 
 try:
-    connection: str = sys.argv[0]
-    headers: str = sys.argv[1]
 
-    df = pd.read_csv(connection)
+    connection: str = sys.argv[0]  # connection path
+    headers: str = sys.argv[1]  # headers to search
 
-    df = df[headers.split(",")]
+    condition: bool = True  # search condition
 
-    rtn = [row.to_dict() for _, row in df.iterrows()]
+    if len(sys.argv) == 3:
+        # search continue symbols in condition string
+        pattern = re.compile(r'(\w+)([><=!]+)(\w+)')
 
-    print(rtn)
+        # pattern to find words
+        pattern1 = re.compile(r'[A-Za-z]')
+
+        # matches of first pattern in string condition
+        res = pattern.match(sys.argv[2])
+
+        # list of headers in condition
+        lst = [h for h in res.groups() if pattern1.match(h)]
+
+        # wrapp headers with df[]
+        wrap = [f"df[{h}]" for h in lst]
+
+        dct = {}  # wrap dictionaries
+        for i, val in enumerate(lst):  # iterate through lst
+            dct[val] = wrap[i]
+
+        res = [dct.get(val, val) for val in res]
+
+        res = "".join(res)
+
+        exec(res)
+
+    df = pd.read_csv(connection)  # data frame
+
+    df = df[headers.split(",")]  # split headers
+
+    rtn = [row.to_dict() for _, row in df.iterrows()]  # convert to list
+
+    print(rtn)  # return
 
 except IndexError:
     print(f"Numero de argumentos incorrecto. Se esperaban 2 y se recibieron {len(sys.argv)}")

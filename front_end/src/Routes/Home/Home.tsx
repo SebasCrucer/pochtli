30c9fe87131d552c5ae023b2ce8212c1510ac2b9
Yael <rayacova37@gmail.com>
2024-04-20 05:11:24 -0600

    remerge

--- front_end/src/Routes/Home/Home.tsx
+++ front_end/src/Routes/Home/Home.tsx
@@ -2,8 +2,7 @@ import { useLoaderData, useNavigation } from 'react-router-dom';
 import './Home.css'
 import { homeLoaderData } from './homeLoader';
 import { PageContainer } from '../../components/PageContainer';
-import {ControlsCard} from '../../components/ControllsCard';
-import SnippedCard from '../../components/SnippedCard';
+
 
 export const Home = () => {
 
@@ -12,12 +11,7 @@ export const Home = () => {
 
     return (
         <section id='Home' className={state === 'loading' ? 'page-loading' : ''}>
-            <SnippedCard 
-            objeto={'Provedor'}
-            obj1={"Filtrar"}
-            icon={<box-icon name="user"/>}
-            icon2={<box-icon name="filter-alt"/>}
-            />
+            <></>
         </section>
     )
 }

57849d7d7d3a1e15e68c664fc7875ad086f31722
Yael <rayacova37@gmail.com>
2024-04-19 23:10:22 -0600

    add: icons Snipped

--- front_end/src/Routes/Home/Home.tsx
+++ front_end/src/Routes/Home/Home.tsx
@@ -3,6 +3,7 @@ import './Home.css'
 import { homeLoaderData } from './homeLoader';
 import { PageContainer } from '../../components/PageContainer';
 import {ControlsCard} from '../../components/ControllsCard';
+import SnippedCard from '../../components/SnippedCard';
 
 export const Home = () => {
 
@@ -11,7 +12,12 @@ export const Home = () => {
 
     return (
         <section id='Home' className={state === 'loading' ? 'page-loading' : ''}>
-            <></>
+            <SnippedCard 
+            objeto={'Provedor'}
+            obj1={"Filtrar"}
+            icon={<box-icon name="user"/>}
+            icon2={<box-icon name="filter-alt"/>}
+            />
         </section>
     )
 }

41273ff03d903c710ff75e21890882b6e8c8d6af
ZiftK <alvaro.cab.lag@gmail.com>
2024-04-20 03:02:24 -0600

    Refact: condition text logic

--- api/query.py
+++ api/query.py
@@ -14,30 +14,8 @@ try:
     df = df[headers.split(",")]  # split headers
 
     if len(sys.argv) == 3:
-        # search continue symbols in condition string
-        pattern = re.compile(r'(\w+)([><=!]+)(\w+)')
 
-        # pattern to find words
-        pattern1 = re.compile(r'[A-Za-z]')
-
-        # matches of first pattern in string condition
-        res = pattern.match(sys.argv[2])
-
-        # list of headers in condition
-        lst = [h for h in res.groups() if pattern1.match(h)]
-
-        # wrapp headers with df[]
-        wrap = [f"df[{h}]" for h in lst]
-
-        dct = {}  # wrap dictionaries
-        for i, val in enumerate(lst):  # iterate through lst
-            dct[val] = wrap[i]
-
-        res = [dct.get(val, val) for val in res]
-
-        res = "".join(res)
-
-        df = df[exec(res)]
+        exec(sys.argv[2])
 
     rtn = [row.to_dict() for _, row in df.iterrows()]  # convert to list
 
